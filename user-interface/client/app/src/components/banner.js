508b2b49876ff3c1cac217515f470c0f27f69c89
sjulian4 <sarah.julian@stonybrook.edu>
2026-08-19 19:50:57 -0400

    banner

---
+++ user-interface/client/app/src/components/banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import '../stylesheets/banner.css';
+
+import sbu_logo from "../sbu-logo.png";
+
+export default class Banner extends React.Component {
+    render(){
+
+        return (
+            <div id="bannerAlignment">
+                <img src={sbu_logo} className="nav" alt="sbu-logo" style={{ width: "5%", height: "5%" }} onClick={() => this.props.toHomePage()}></img>
+                <h1 id="bannerTitle" className="nav" onClick={this.props.toHomePage}>TDF-DFT for QM7 Molecules</h1>
+                <button type="button" className={this.props.page === "home_page" ? "selected" : "navButtons"} onClick={() => this.props.toHomePage()}>Home</button>
+                <button type="button" className={this.props.page === "database_page" ? "selected" : "navButtons"} onClick={() => this.props.toDatabasePage()}>Database</button>
+                <button type="button" className={this.props.page === "about_page" ? "selected" : "navButtons"} onClick={() => this.props.toAboutPage()}>About</button>
+                <button type="button" className={this.props.page === "help_page" ? "selected" : "navButtons"} onClick={() => this.props.toHelpPage()}>Help</button>
+                <button type="button" className={this.props.page === "contact_us_page" ? "selected" : "navButtons"} onClick={() => this.props.toContactUsPage()}>Contact Us</button>
+                <button type="button" className={this.props.page === "settings_page" ? "selected" : "navButtons"} onClick={() => this.props.toSettingsPage()}>Settings</button>
+                        
+            </div>
+        );
+    }
+}
--- user-interface/client/app/src/components/website.js
+++ user-interface/client/app/src/components/website.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import {useState} from 'react';
+
+import Banner from "./banner.js";
+
 export default function Website() {
+    const [page,setPage] = useState("home_page");
+
     return(
             <div style={{display:"flex"}}>
-            <div style={{color:"tomato",fontWeight:"bold", display:"flex"}}>
-            <h1 >{"placeholder"}</h1>
-            </div>
+            <Banner page={page} toHomePage={() => setPage("home_page")} toDatabasePage={() => setPage("database_page")} toAboutPage={() => setPage("about_page")} toHelpPage={() => setPage("help_page")} toContactUsPage={() => setPage("contact_us_page")} toSettingsPage={() => setPage("settings_page")}/>
             </div>
                 
         );

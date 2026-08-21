8e89807b4b6a72f8773f86214b6ef7ce4e0c31d0
sjulian4 <sarah.julian@stonybrook.edu>
2026-08-21 13:45:39 -0400

    updating with linter

--- user-interface/client/app/src/components/website.js
+++ user-interface/client/app/src/components/website.js
@@ -10,9 +10,9 @@ export default function Website() {
     const [page,setPage] = useState("home_page");
 
     return(
-            <div id="overall_website">
+            <div id="overall-website">
                 <Banner page={page} toHomePage={() => setPage("home_page")} toDatabasePage={() => setPage("database_page")} toAboutPage={() => setPage("about_page")} toHelpPage={() => setPage("help_page")} toContactUsPage={() => setPage("contact_us_page")} toSettingsPage={() => setPage("settings_page")}/>
-                <div id="page_view">
+                <div id="page-view">
                     <Page page={page}/> 
                 </div>
             </div>

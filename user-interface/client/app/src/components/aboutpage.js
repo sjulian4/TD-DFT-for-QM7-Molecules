1bed03bda1e5ee573f36349662a95f83823919c0
sjulian4 <sarah.julian@stonybrook.edu>
2026-08-23 13:53:42 -0400

    correcting formatting for p

--- user-interface/client/app/src/components/aboutpage.js
+++ user-interface/client/app/src/components/aboutpage.js
@@ -8,10 +8,10 @@ export default class AboutPage extends React.Component {
                 <h1 id="about-title">About the TD-DFT for QM7 Molecules Web Application</h1>
 
                 <hr />
-                
+
                 <div>
                     <h2 class="section-title">The goal of this application</h2>
-                    <p>This application was created to provide a user-friendly interface for exploring the properties of QM7 molecules using Time-Dependent Density Functional Theory (TD-DFT) calculations.</p>
+                    <p class="info-text">This application was created to provide a user-friendly interface for exploring the properties of QM7 molecules using Time-Dependent Density Functional Theory (TD-DFT) calculations.</p>
                 </div>
 
                 <hr />
@@ -20,17 +20,17 @@ export default class AboutPage extends React.Component {
                     <h2 class="section-title">Context</h2>
 
                     <h3 class="subsection-title">What is TD-DFT?</h3>
-                    <p>Time-Dependent Density Functional Theory (TD-DFT) is a computational method used to calculate the excited states of molecules by solving the time-dependent Kohn-Sham equations.</p>
+                    <p class="info-text">Time-Dependent Density Functional Theory (TD-DFT) is a computational method used to calculate the excited states of molecules by solving the time-dependent Kohn-Sham equations.</p>
 
                     <h3 class="subsection-title">What are QM7 molecules?</h3>
-                    <p>QM7 is a dataset of 7,165 organic molecules with 10 heavy atoms each, used for benchmarking quantum chemical methods.</p>
+                    <p class="info-text">QM7 is a dataset of 7,165 organic molecules with 10 heavy atoms each, used for benchmarking quantum chemical methods.</p>
                 </div>
 
                 <hr />
 
                 <div>
                     <h2 class="section-title">What can you do on this site?</h2>
-                    <p>placeholder how to use/features</p>
+                    <p class="info-text">placeholder how to use/features</p>
 
                 </div>
 
@@ -38,7 +38,7 @@ export default class AboutPage extends React.Component {
 
                 <div>
                     <h2 class="section-title">How this application was developed</h2>
-                    <p>placeholder development</p>
+                    <p class="info-text">placeholder development</p>
 
                 </div>
 
